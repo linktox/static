6a986e85a1168ef07457a73a48f8d680f3df17ff
LinkToX <web3linktox@gmail.com>
2023-10-08 08:53:29 +0800

    add erc methods

--- abi.js
+++ abi.js
@@ -75,6 +75,9 @@ window.ABIS = {
   "IERC20": [
     "event Approval(address indexed,address indexed,uint256)",
     "event Transfer(address indexed,address indexed,uint256)",
+    "function name() view returns (string)",
+    "function symbol() view returns (string)",
+    "function decimals() view returns (uint8)",
     "function allowance(address,address) view returns (uint256)",
     "function approve(address,uint256) returns (bool)",
     "function balanceOf(address) view returns (uint256)",
